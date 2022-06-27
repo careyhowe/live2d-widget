785c40b548855abcd01a5391cf2722c5cf2f50b3
careyhowe <careyhowe@163.com>
2022-06-27 13:09:33 +0800

    改

--- autoload.js
+++ autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
-//const live2d_path = "/live2d-widget/";
+//const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
+const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
@@ -61,3 +61,31 @@ console.log(`
                   ﾄ-,/  |___./
                   'ｰ'    !_,.:
 `);
+
+// 加载 CSS
+$("<link>").attr({href: "https://www.example.com/path/to/waifu.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
+
+// 插入 DIV
+$('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
+
+// 加载 JS
+$.ajax({
+	url: 'https://www.example.com/path/to/waifu-tips.js',
+	dataType:"script",
+	cache: true,
+	async: false
+});
+$.ajax({
+	url: 'https://www.example.com/path/to/live2d.js',
+	dataType:"script",
+	cache: true,
+	async: false
+});
+
+// 初始化看板娘，加载 waifu-tips.json
+
+/* 可直接修改部分参数 */
+live2d_settings['modelId'] = 5;                  // 默认模型 ID
+live2d_settings['modelTexturesId'] = 1;          // 默认材质 ID
+/* 在 initModel 前添加 */
+initModel('https://www.example.com/path/to/waifu-tips.json');
